6b69c38c333b8c71006a221325776b6b78f4e852
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2024-02-14 22:57:13 +0200

    add home

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./components/App/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/rental-car">
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import Image from "../../img/homepage.jpg";
+import { Link } from "react-router-dom";
+
+export const Wrapper = styled.div``;
+
+export const Text = styled.p``;
+
+export const Button = styled(Link)``;
