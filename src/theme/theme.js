7bac598c4ab61fb662540b7310e9521b67d23edf
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2024-02-14 09:06:51 +0200

    initial rep

---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+export const theme = Object.freeze({
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    grey: '#546170',
+    lightGrey: '#f5fafc',
+    blue: '#e3f0ff',
+    accentBlue: '#86b1de',
+    primaryText: '#37373d',
+    secondaryText: '#63788e',
+    pink: '#af6ba7',
+    yellow: '#ece990',
+    banana: '#ffe135',
+    tomato: '#ff6347',
+    green: '#abec90',
+    orange: '#ffbe76',
+    red: '#ff4411',
+  },
+});
