009cf1bd8bc567b9f57c32e6299192182877c989
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2024-02-20 22:02:01 +0200

    navbar add home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,27 @@
+import NavBar from "components/NavBar/NavBar";
 import { Button, Text, Wrapper, Hero } from "./Home.styled";
 
 export default function Home() {
   return (
-    <Wrapper>
-      <Hero>
-        New Car Rental in Ukraine with Address Delivery within an Hour
-      </Hero>
-      <Text>
-        Car rental is the best way to move around million-strong cities
-        regardless of the time of year. RENTAL company provides car rental
-        services in all major cities of Ukraine. Our branches are located in
-        Kyiv, Kharkiv, Dnipro, Lviv, Odesa, Vinnytsia, Zaporizhzhia, and
-        Ivano-Frankivsk. RENTAL services are a wide range of cars for rent, from
-        economy cars such as Chevrolet Spark to premium sedans like Mercedes S
-        Class and SUVs like Audi Q7 and Toyota Land Cruiser 200. When you rent a
-        car on our website, you choose the specific model, not the class of the
-        car.
-      </Text>
-      <Button to={"/catalog"}>Rental cars</Button>
-    </Wrapper>
+    <>
+      <NavBar />
+      <Wrapper>
+        <Hero>
+          New Car Rental in Ukraine with Address Delivery within an Hour
+        </Hero>
+        <Text>
+          Car rental is the best way to move around million-strong cities
+          regardless of the time of year. RENTAL company provides car rental
+          services in all major cities of Ukraine. Our branches are located in
+          Kyiv, Kharkiv, Dnipro, Lviv, Odesa, Vinnytsia, Zaporizhzhia, and
+          Ivano-Frankivsk. RENTAL services are a wide range of cars for rent,
+          from economy cars such as Chevrolet Spark to premium sedans like
+          Mercedes S Class and SUVs like Audi Q7 and Toyota Land Cruiser 200.
+          When you rent a car on our website, you choose the specific model, not
+          the class of the car.
+        </Text>
+        <Button to={"/catalog"}>Rental cars</Button>
+      </Wrapper>
+    </>
   );
 }
